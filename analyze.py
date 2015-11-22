6e405948d8c226385d55b9968fc99fbc34024de8
quantwiz <quantwiz@gmail.com>
2015-11-21 23:44:53 -0500

    Create analyze.py

---
+++ analyze.py
@@ -0,0 +1,69 @@
+#www.laurentluce.com/posts/twitter-sentiment-analysis-using-pythondf-and-nltk/
+import nltk
+from nltk.probability import ELEProbDist
+
+pos_tweets = [('I love this car', 'positive'),
+              ('This view is amazing', 'positive'),
+              ('I feel great this morning', 'positive'),
+              ('I am so excited about the concert', 'positive'),
+              ('He is my best friend', 'positive')]
+neg_tweets = [('I do not like this car', 'negative'),
+              ('This view is horrible', 'negative'),
+              ('I feel tired this morning', 'negative'),
+              ('I am not looking forward to the concert', 'negative'),
+              ('He is my enemy', 'negative')]
+tweets = []
+for (words, sentiment) in pos_tweets + neg_tweets:
+    words_filtered = [e.lower() for e in words.split() if len(e) >= 3] 
+    tweets.append((words_filtered, sentiment))
+
+test_tweets = [
+    (['feel', 'happy', 'this', 'morning'], 'positive'),
+    (['larry', 'friend'], 'positive'),
+    (['not', 'like', 'that', 'man'], 'negative'),
+    (['house', 'not', 'great'], 'negative'),
+    (['your', 'song', 'annoying'], 'negative')]
+
+def get_words_in_tweets(tweets):
+    all_words = []
+    for (words, sentiment) in tweets:
+      all_words.extend(words)
+    return all_words
+
+def get_word_features(wordlist):
+    wordlist = nltk.FreqDist(wordlist)
+    word_features = wordlist.keys()
+    return word_features
+
+def extract_features(document):
+    document_words = set(document)
+    features = {}
+    for word in word_features:
+        features['contains(%s)' % word] = (word in document_words)
+    return features
+
+
+def train(labeled_featuresets, estimator=ELEProbDist):
+    
+    # Create the P(label) distribution
+    label_probdist = estimator(label_freqdist)
+    
+    # Create the P(fval|label, fname) distribution
+    feature_probdist = {}
+
+    return NaiveBayesClassifier(label_probdist, feature_probdist)
+
+
+word_features = get_word_features(get_words_in_tweets(tweets))
+
+training_set = nltk.classify.apply_features(extract_features, tweets)
+
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+tweet = 'Larry is not my friend'
+print extract_features(tweet.split())
+print classifier.classify(extract_features(tweet.split()))
+
+
+
+
